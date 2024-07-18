f007c13b8ad767381ef221f40b6af50738e584fc
lhtlikeAyaka <125693369+lhtlikeAyaka@users.noreply.github.com>
2024-07-18 22:56:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,18 +17,17 @@ const BLOG = {
   IS_TAG_COLOR_DISTINGUISHED:
     process.env.NEXT_PUBLIC_IS_TAG_COLOR_DISTINGUISHED === 'true' || true, // 对于名称相同的tag是否区分tag的颜色
 
-  // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
+   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，欢迎来到这里,这是我的个人网站,一个高中生的个人网站!',
+    'Hi，我是一个高中生,Hi，我是一个干饭人,欢迎来到我的博客🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'lht' // 您的昵称 例如 tangly1024
-  bio: process.env.NEXT_PUBLIC_BIO || '一个普通的学生' // 作者简介
-  link: process.env.NEXT_PUBLIC_LINK || 'https://994528.xyz' // 网站地址
-  keyword: process.env.NEXT_PUBLIC_KEYWORD || 'Notion,博客,游戏,生活,二次元' // 网站关键词 英文逗号隔开
-
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'lht', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的学生', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://994528.xyz', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
