79c92038393c1c7248a862344ece49346967e4bb
lhtlikeAyaka <125693369+lhtlikeAyaka@users.noreply.github.com>
2024-07-18 22:43:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'lht' // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的学生' // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || '994528.xyz' // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://994528.xyz' // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion,博客,游戏,生活,二次元' // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
